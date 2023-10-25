b22850d294be3e797830032e9b981b49c4c78281
vKurama7u7v <aurelio.marin@iest.edu.mx>
2023-10-24 19:37:36 -0600

    feat: update Examples Section :construction:

--- screens/Home/HomeScreen.jsx
+++ screens/Home/HomeScreen.jsx
@@ -3,16 +3,17 @@ import React from "react";
 import BaseLayout from "@/layouts/BaseLayout";
 import AboutMe from "@/sections/AboutMe";
 import MusicPlayerWidget from "@/widgets/music-player/MusicPlayerWidget";
+import ExampleSection from "@/sections/Examples/ExampleSection";
 
 function HomeScreen() {
   return (
     <>
       <BaseLayout>
-        <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <MusicPlayerWidget />
-          </div>
-        </section>
+        {/* Examples */}
+        <ExampleSection>
+          <MusicPlayerWidget />
+        </ExampleSection>
+
         <AboutMe />
       </BaseLayout>
     </>
--- sections/AboutMe/AboutMeSection.jsx
+++ sections/AboutMe/AboutMeSection.jsx
@@ -5,8 +5,8 @@ function AboutMeSection() {
     <>
       <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-col">
-          <div class="lg:w-4/6 mx-auto">
-            <div class="rounded-lg h-64 overflow-hidden">
+          <div class="lg:w-5/6 mx-auto">
+            <div class="rounded-lg h-[18em] overflow-hidden">
               <img
                 src="https://dummyimage.com/1200x500"
                 alt="content"
@@ -15,23 +15,16 @@ function AboutMeSection() {
             </div>
             <div class="flex flex-col sm:flex-row mt-10">
               <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-10 h-10"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
+                <div class="w-[80px] h-[80px] rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400 overflow-hidden">
+                  <img
+                    src="https://res.cloudinary.com/doi13m5h7/image/upload/v1674939022/portfolio/photo_Aurelio_Marin_Bautista.png"
+                    alt=""
+                    class="object-cover object-center"
+                  />
                 </div>
                 <div class="flex flex-col items-center text-center justify-center">
                   <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
-                    Phoebe Caulfield
+                    vKurama7u7v
                   </h2>
                   <div class="w-12 h-1 bg-green-500 rounded mt-2 mb-4"></div>
                   <p class="text-base">
